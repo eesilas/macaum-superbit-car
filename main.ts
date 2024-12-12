7de85e38a4231394b639f1bf0357add55f0209f7
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-12-12 10:03:01 +0800

    Forward movement.

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
 basic.forever(function () {
-	
+    SuperBit.MotorRunDual(
+    SuperBit.enMotors.M1,
+    128,
+    SuperBit.enMotors.M2,
+    128
+    )
+    SuperBit.MotorRunDual(
+    SuperBit.enMotors.M3,
+    128,
+    SuperBit.enMotors.M4,
+    128
+    )
 })
